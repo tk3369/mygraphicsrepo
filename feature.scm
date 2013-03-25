d8e74893241818f5681ff669c2b9def83a8df1c1
Tom <tk3369@gmail.com>
2013-03-24 23:32:53 -0700

    fixed ))

--- feature.scm
+++ feature.scm
@@ -30,6 +30,6 @@
 		    ;; hexagonal Gosper curve - page 12
 		    ((4) (draw-koch-curve 8                 ;; curve type
 					  (if iphone? 5 10) ;; length
-					  3                 ;; level))
+					  3))               ;; level
 
 		    ))))
