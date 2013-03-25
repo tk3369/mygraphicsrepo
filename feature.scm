e50168c445111423297fc0b909785c4b531bf97d
Tom <tk3369@gmail.com>
2013-03-24 23:15:22 -0700

    test

--- feature.scm
+++ feature.scm
@@ -19,10 +19,10 @@
 
 		    ;; custom pattern
 		    ((3) (draw-polygon-spiral (if phone? 6 15)                  ;; length
-					      5      ;; sides
-					      13     ;; angle
-					      3      ;; side increase per iteration
-					      50))
+					      3      ;; sides
+					      8      ;; angle
+					      2      ;; side increase per iteration
+					      40))
 
 		    ;; hexagonal Gosper curve - page 12
 		    ((4) (draw-koch-curve 9 (if iphone? 5 10) 3))
