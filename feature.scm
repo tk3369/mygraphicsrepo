2682f66ad9ae678addbb9c6408a0f16cee95ee29
tk3369 <tk3369@gmail.com>
2013-03-25 04:11:01 -0700

    test

--- feature.scm
+++ feature.scm
@@ -37,7 +37,7 @@
 		    ;; koch - page 10
 		    ((5) (draw-lsys-graph '(f - f - f - f)
 					  '((f f f - f + f - f - f f))
-					  (if phone? 5 20)  ;; length
+					  (if phone? 8 20)  ;; length
 					  90                ;; angle
 					  3))               ;; level
 
